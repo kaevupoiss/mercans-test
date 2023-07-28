34b2f3efc6832e9cda5edd4fcd8ec051e062c812
Rasmus Talioja <rasmus.talioja@gmail.com>
2023-07-28 19:08:54 +0300

    Add payslips view

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,205 +1,228 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { RouterLink } from "vue-router";
-import menuLinks from '@/data/menuLinks.json';
-import IconComponent from '@/components/IconComponent.vue';
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+import menuLinks from '@/data/menuLinks.json'
+import IconComponent from '@/components/IconComponent.vue'
 
-const menuOpen = ref(false);
-const toggleMenu = () => menuOpen.value = !menuOpen.value;
+const menuOpen = ref(false)
+const toggleMenu = () => (menuOpen.value = !menuOpen.value)
 
-const filteredMenuLinks = menuLinks.filter(link => link.enabled).sort((a, b) => a.ordinal - b.ordinal);
+const filteredMenuLinks = menuLinks
+  .filter((link) => link.enabled)
+  .sort((a, b) => a.ordinal - b.ordinal)
 
-//TODO: Add sidemenu collapse/expand functionality
+//TODO: Add sidemenu collapse/expand functionality - Not in design.
+
+const TRANSLATIONS: Record<string, string> = {
+  quick_access: 'Quick access',
+  time_entry: 'Time Entry',
+  my_information: 'My Information',
+  my_team: 'My Team',
+  payslips: 'My Payslips',
+  leaves: 'Leave',
+  organizational_chart: 'Organizational chart',
+  expense: 'Benefits',
+  documents: 'Documents'
+}
+
+const t = (k: string) => {
+  return TRANSLATIONS[k] ?? k
+}
 </script>
 
 <template>
-    <div class="container">
-        <div class="sidemenu">
-            <div class="sidemenu__header">
-                <p>Employee <br> self service</p>
-            </div>
-            <div class="sidemenu__links">
-                <RouterLink v-for="link in filteredMenuLinks" :to="link.path ?? '/'" :key="link.ordinal + 'menuitem'">
-                    <IconComponent :name="link.icon" :height="parseInt(link.height)" :width="parseInt(link.width)" />
-                    {{ link.title }}
-                </RouterLink>
-            </div>
-            <div class="sidemenu__footer">
-                <p>
-                    Mercans © 2021
-                </p>
-            </div>
-            <button class="sidemenu__toggle" @click="toggleMenu">
-                <div class="icon-wrapper">
-                    <IconComponent name="chevron" :height="10" :width="10" :rotation="menuOpen ? 0 : 180" />
-                </div>
-            </button>
-        </div>
-        <div class="content">
-            <slot></slot>
-        </div>
+  <div class="container">
+    <div class="sidemenu">
+      <div class="sidemenu__header">
+        <p>
+          Employee <br />
+          self service
+        </p>
+      </div>
+      <div class="sidemenu__links">
+        <RouterLink
+          v-for="link in filteredMenuLinks"
+          :to="link.path ?? '/'"
+          :key="link.ordinal + 'menuitem'"
+        >
+          <IconComponent
+            :name="link.icon"
+            :height="parseInt(link.height)"
+            :width="parseInt(link.width)"
+          />
+          {{ t(link.title) }}
+        </RouterLink>
+      </div>
+      <div class="sidemenu__footer">
+        <p>Mercans © 2021</p>
+      </div>
+      <button class="sidemenu__toggle" @click="toggleMenu">
+        <span class="icon-wrapper">
+          <IconComponent name="chevron" :height="10" :width="10" :rotation="menuOpen ? 0 : 180" />
+        </span>
+      </button>
     </div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
 </template>
-
 
 <style scoped>
 .container {
-    display: grid;
-    grid-template-columns: min-content 1fr;
-    grid-template-rows: 1fr;
-    width: 100%;
+  display: grid;
+  grid-template-columns: max-content 1fr;
+  grid-template-rows: 1fr;
+  width: 100%;
 }
 
 .sidemenu {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    background-color: var(---ffffff);
+  background-color: var(---ffffff);
 
-    position: relative;
+  position: relative;
 
-    border-right: 1px solid var(---97a0ac);
+  border-right: 1px solid var(---97a0ac);
 
-    z-index: 10;
+  z-index: 10;
 }
 
 .sidemenu__header {
-    width: 100%;
+  width: 100%;
 
-    background-color: var(--black);
+  background-color: var(--black);
 
-    padding: 1.25rem 0 1.125rem 2.25rem;
+  padding: 1.25rem 0 1.125rem 2.25rem;
 }
 
 .sidemenu__header p {
-    color: var(---ffffff);
+  color: var(---ffffff);
 
-    font-size: 0.875rem;
-    line-height: 1;
-    font-weight: bold;
+  font-size: 0.875rem;
+  line-height: 1;
+  font-weight: bold;
 }
 
 .sidemenu__links {
-    display: flex;
-    flex-direction: column;
-    padding: 2rem 0;
-
+  display: flex;
+  flex-direction: column;
+  padding: 2rem 0;
 }
 
 .sidemenu__links a {
-    display: grid;
-    grid-template-columns: 1.5rem 1fr;
-    align-items: center;
-    column-gap: 1rem;
-    padding: 0.5rem 2.25rem;
+  display: grid;
+  grid-template-columns: 1.5rem 1fr;
+  align-items: center;
+  column-gap: 1rem;
+  padding: 0.5rem 2.25rem;
 
-    position: relative;
+  position: relative;
 
-    font-size: 0.875rem;
-    line-height: 1.5;
+  font-size: 0.875rem;
+  line-height: 1.5;
 
-    text-decoration: none;
+  text-decoration: none;
 
-    transition: color 100ms ease-in-out;
+  transition: color 100ms ease-in-out;
 }
 
 .sidemenu__links a.router-link-active {
-    background-color: var(--light-grayish-blue);
-    color: var(--orange);
+  background-color: var(--light-grayish-blue);
+  color: var(--orange);
 }
 
 .sidemenu__links a.router-link-active::before {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    width: 0.25rem;
-    background-color: var(--orange);
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 0.25rem;
+  background-color: var(--orange);
 }
 
 .sidemenu__links a svg {
-    place-self: center;
+  place-self: center;
 }
 
 .sidemenu__links a :deep(svg *) {
-    fill: var(--black);
+  fill: var(--black);
 
-    transition: fill 100ms ease-in-out;
+  transition: fill 100ms ease-in-out;
 }
 
 .sidemenu__links a.router-link-active :deep(svg *) {
-    fill: var(--orange);
+  fill: var(--orange);
 }
 
 .sidemenu__footer {
-    display: flex;
-    justify-content: center;
-    margin-top: auto;
-    padding: 1.5rem 0;
+  display: flex;
+  justify-content: center;
+  margin-top: auto;
+  padding: 1.5rem 0;
 }
 
 .sidemenu__toggle {
-    position: absolute;
-    width: 2.5rem;
-    height: 2.5rem;
+  position: absolute;
+  width: 2.5rem;
+  height: 2.5rem;
 
-    top: 3rem;
-    right: -1.25rem;
+  top: 3rem;
+  right: -1.25rem;
 
-    display: flex;
-    align-items: center;
-    justify-content: end;
+  display: flex;
+  align-items: center;
+  justify-content: end;
 
-    padding-right: 0.5rem;
+  padding-right: 0.5rem;
 
-    appearance: none;
-    border: none;
-    background-color: var(--light-grayish-blue);
-    border-radius: 9999px;
+  appearance: none;
+  border: none;
+  background-color: var(--light-grayish-blue);
+  border-radius: 9999px;
 }
 
 .sidemenu__toggle .icon-wrapper {
-    display: grid;
-    place-items: center;
+  display: grid;
+  place-items: center;
 
-    width: 1.5rem;
-    height: 1.5rem;
+  width: 1.5rem;
+  height: 1.5rem;
 
-    border-radius: 9999px;
-    background-color: var(---ffffff);
+  border-radius: 9999px;
+  background-color: var(---ffffff);
 
-    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.29);
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.29);
 
-    transition: width 300ms ease-out;
+  transition: width 300ms ease-out;
 }
 
 .sidemenu__toggle .icon-wrapper :deep(svg) {
-    translate: 0;
+  translate: 0;
 
-    transition: translate 300ms ease-out, rotate 300ms ease-out;
+  transition: translate 300ms ease-out, rotate 300ms ease-out;
 }
 
 .sidemenu__toggle .icon-wrapper :deep(svg *) {
-    fill: var(--black);
+  fill: var(--black);
 }
 
 .sidemenu__toggle:hover {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .sidemenu__toggle:hover .icon-wrapper {
-
-    background-color: var(--black);
-    width: 1.75rem;
+  background-color: var(--black);
+  width: 1.75rem;
 }
 
 .sidemenu__toggle:hover .icon-wrapper :deep(svg) {
-
-    translate: -0.5rem;
+  translate: -0.5rem;
 }
 
 .sidemenu__toggle:hover .icon-wrapper :deep(svg *) {
-    fill: var(--orange);
+  fill: var(--orange);
 }
 </style>
--- src/views/PayslipView.vue
+++ src/views/PayslipView.vue
@@ -1,11 +1,530 @@
+<script setup lang="ts">
+import IconComponent from '@/components/IconComponent.vue'
+import payslips from '@/data/payslips.json'
+import { format } from 'date-fns'
+import { computed, ref } from 'vue'
+
+const tabs = ref(
+  payslips.reduce((acc: Map<string, number>, cur) => {
+    const key = cur.payslipEntries[0]?.currency
+
+    const val = acc.get(key)
+    acc.set(key, val ? val + 1 : 1)
+
+    return acc
+  }, new Map<string, number>())
+)
+
+// If there are no tabs then fall back to USD
+const selectedTab = ref(tabs.value.keys().next().value ?? 'USD')
+
+// TODO: onSelectedTabChange
+
+const filteredPayslips = computed(() =>
+  payslips.filter((payslip) => payslip.payslipEntries[0]?.currency === selectedTab.value)
+)
+
+const modalOpen = ref(false)
+const toggleModal = () => (modalOpen.value = !modalOpen.value)
+
+const openPdfIndex = ref<number | undefined>()
+
+const onRowToggle = (index: number) => {
+  if (openPdfIndex.value === index) {
+    openPdfIndex.value = undefined
+  } else {
+    openPdfIndex.value = index
+  }
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is the payslips page</h1>
+  <div class="container">
+    <h1>My Payslips</h1>
+    <div class="tabs">
+      <button
+        class="tab"
+        :class="{ active: tab === selectedTab }"
+        v-for="tab in tabs.keys()"
+        :key="'tab' + tab"
+        @click="selectedTab = tab"
+      >
+        {{ tab }} ({{ tabs.get(tab) }})
+      </button>
+    </div>
+    <div class="table">
+      <div class="table__header table__row">
+        <div class="table__cell">
+          <p>#</p>
+        </div>
+        <div class="table__cell">
+          <p>Month</p>
+        </div>
+        <div class="table__cell">
+          <p>Payslip</p>
+        </div>
+        <div class="table__cell">
+          <p>Gross salary</p>
+        </div>
+        <div class="table__cell">
+          <p>Net pay</p>
+        </div>
+      </div>
+      <div
+        v-for="(payslip, index) in filteredPayslips"
+        :key="'payslip' + payslip.fileAttachment?.id"
+        class="table__row"
+        @click="() => onRowToggle(index)"
+        :class="openPdfIndex === index ? 'expanded' : ''"
+      >
+        <div class="table__cell">
+          <p>{{ index + 1 }}</p>
+        </div>
+        <div class="table__cell table__cell--month">
+          <button
+            class="collapse tooltip"
+            :data-tooltip="openPdfIndex === index ? 'Close' : 'Expand'"
+          >
+            <IconComponent
+              name="chevron"
+              :height="8"
+              :width="8"
+              :rotation="openPdfIndex === index ? 270 : 90"
+            />
+            <IconComponent
+              name="chevron"
+              :height="8"
+              :width="8"
+              :rotation="openPdfIndex === index ? 90 : 270"
+            />
+          </button>
+          <p>{{ format(new Date(payslip.payrollDate), 'MMMM yyyy') }}</p>
+        </div>
+        <div class="table__cell">
+          <p>{{ payslip.fileAttachment?.file?.label }}</p>
+        </div>
+        <div class="table__cell">
+          <p>
+            {{ payslip.payslipEntries?.find((el) => el.key === 'GROSS')?.amount.toFixed(2) }}
+            {{ selectedTab }}
+          </p>
+        </div>
+        <div class="table__cell">
+          <p>
+            {{ payslip.payslipEntries?.find((el) => el.key === 'NET PAY')?.amount.toFixed(2) }}
+            {{ selectedTab }}
+          </p>
+        </div>
+        <div class="pdf-container">
+          <Transition :duration="300">
+            <iframe
+              v-if="openPdfIndex === index"
+              :src="'payslipsFiles/' + payslip.fileAttachment?.accessToken + '.pdf'"
+            ></iframe>
+          </Transition>
+        </div>
+      </div>
+      <button
+        class="salary-evaluation tooltip"
+        @click="toggleModal"
+        data-tooltip="View salary evolution"
+      >
+        <span class="icon-wrapper">
+          <IconComponent name="graph" :height="24" :width="24" />
+        </span>
+      </button>
+    </div>
+
+    <Transition :duration="300" name="modal">
+      <div v-if="modalOpen" class="overlay">
+        <div class="modal">
+          <div class="modal__header">
+            <h2>Gross/Net Salary evolution</h2>
+            <button class="modal__close" @click="toggleModal">
+              <IconComponent name="close" :height="24" :width="24" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <style scoped>
-div {
-  padding: 2rem 5rem;
+h1 {
+  color: var(--black);
+  font-size: 1.5rem;
+  font-weight: bold;
+  line-height: 1.5;
+
+  margin-left: 1.5rem;
+}
+
+.container {
+  padding: 2rem 4.5rem;
+}
+
+.tabs {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+
+  margin-top: 1.875rem;
+  padding-left: 1.5rem;
+  height: 3.5rem;
+  background-color: var(--black);
+  border-radius: 0.5rem 1.75rem 1.75rem 0.5rem;
+
+  z-index: 1;
+}
+
+.tab {
+  appearance: none;
+  border: none;
+  display: grid;
+  place-items: center;
+
+  background-color: var(--black);
+  color: var(---ffffff);
+  font-size: 0.875rem;
+  line-height: 1rem;
+  font-weight: bold;
+  padding: 1rem;
+
+  transition: color 0.1s ease-out, background-color 0.1s ease-out;
+}
+
+.tab.active {
+  background-color: var(--orange);
+}
+
+.tab:hover {
+  color: var(--orange);
+}
+
+.tab.active:hover {
+  color: var(---ffffff);
+}
+
+.table {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  background-color: var(---ffffff);
+  padding: 2.5rem 4.5rem 5rem 4.5rem;
+  margin-top: -1.5rem;
+  border-radius: 0 0 0.75rem 0.75rem;
+
+  box-shadow: 0px 3px 6px #fee5db4d;
+  border: 1px solid #fee5db;
+
+  --column-gap: 1rem;
+  --table-columns: 3rem 7rem 1fr 7rem 7rem;
+}
+
+.table__header {
+  color: var(--dark-gray);
+  font-size: 0.75rem;
+  line-height: 1;
+  font-weight: bold;
+}
+
+.table__row {
+  display: grid;
+  grid-template-columns: var(--table-columns);
+  grid-template-rows: max-content 0;
+  row-gap: 0.5rem;
+  column-gap: var(--column-gap);
+  padding: 0.75rem 2.5rem;
+  border-bottom: 1px solid var(--light-gray);
+
+  background-color: transparent;
+  transition: background-color 0.3s ease-out, grid-template-rows 0.3s ease-out;
+}
+
+.table__row.expanded {
+  grid-template-rows: max-content 800px;
+}
+
+.table__row:not(.table__header):hover {
+  cursor: pointer;
+
+  background-color: #fee5db;
+}
+
+.table__cell {
+  display: flex;
+  align-content: start;
+  font-size: 0.875rem;
+  line-height: 1.5;
+}
+
+.table__cell--month {
+  height: min-content;
+
+  position: relative;
+
+  color: var(--orange);
+}
+
+button.collapse {
+  position: absolute;
+
+  cursor: pointer;
+
+  appearance: none;
+  border: none;
+  background-color: transparent;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 0;
+
+  width: 1.5rem;
+  height: 1.5rem;
+
+  border-radius: 999px;
+
+  opacity: 0;
+
+  overflow: visible;
+
+  top: -0.125rem;
+
+  transition: all 0.3s ease-out;
+}
+
+button.collapse:hover {
+  background-color: var(--black);
+  height: 2rem;
+  row-gap: 0.5rem;
+
+  top: -0.375rem;
+}
+
+button.collapse:hover :deep(svg *) {
+  fill: var(--orange);
+}
+
+.table__row .table__cell--month p {
+  transition: transform 0.3s ease-out;
+}
+
+.table__row:hover button.collapse,
+.table__row.expanded button.collapse {
+  opacity: 1;
+}
+
+.table__row:hover .table__cell--month p,
+.table__row.expanded .table__cell--month p {
+  transform: translateX(2rem);
+}
+
+.pdf-container {
+  grid-column: 1 / -1;
+  overflow: hidden;
+}
+
+.pdf-container iframe {
+  width: 100%;
+  height: 100%;
+}
+
+.salary-evaluation {
+  appearance: none;
+  border: none;
+  background-color: var(--light-grayish-blue);
+
+  border-radius: 999px;
+
+  cursor: pointer;
+
+  position: absolute;
+  top: 4rem;
+  right: -1.75rem;
+
+  display: grid;
+  place-items: center;
+  width: 3.5rem;
+  height: 3.5rem;
+}
+
+.salary-evaluation .icon-wrapper {
+  display: grid;
+  place-items: center;
+
+  height: 3rem;
+  width: 3rem;
+  border-radius: 999px;
+  background-color: var(---ffffff);
+
+  transition: background-color 0.2s ease-out;
+}
+
+.salary-evaluation:hover .icon-wrapper {
+  background-color: var(---fee5db);
+}
+
+.tooltip::before {
+  pointer-events: none;
+
+  display: block;
+  padding: 0.25rem 1rem;
+  right: calc(100% + 0.5rem);
+
+  content: attr(data-tooltip);
+  white-space: nowrap;
+  color: var(---ffffff);
+  position: absolute;
+
+  font-size: 0.875rem;
+  line-height: 1.5;
+
+  border-radius: 0.25rem;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.29);
+
+  background-color: var(--black);
+}
+
+.tooltip::after {
+  display: block;
+
+  content: '';
+  position: absolute;
+  height: 0;
+  width: 0;
+  border-top: 5px solid transparent;
+  border-left: 4px solid var(--black);
+  border-bottom: 5px solid transparent;
+
+  right: 100%;
+  margin-right: 0.25rem;
+}
+
+.tooltip::before,
+.tooltip::after {
+  opacity: 0;
+  transition: opacity 0.3s ease-out;
+}
+
+.tooltip:hover::before,
+.tooltip:hover::after {
+  opacity: 1;
+}
+
+.collapse.tooltip::before {
+  bottom: 100%;
+  margin-bottom: 0.75rem;
+  left: -1rem;
+  width: max-content;
+}
+
+.collapse.tooltip::after {
+  border-right: 5px solid transparent;
+  border-top: 5px solid var(--black);
+  border-left: 5px solid transparent;
+
+  bottom: 100%;
+  margin-bottom: 0.25rem;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.overlay {
+  position: fixed;
+  inset: 0;
+
+  z-index: 100;
+  background-color: rgba(151, 160, 172, 0.8);
+}
+
+.modal {
+  position: fixed;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  z-index: 101;
+  background-color: var(---ffffff);
+
+  border-radius: 0.75rem 0.75rem 0 0;
+  margin: 2.75rem 2.25rem 0 2.25rem;
+}
+
+.modal__header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 2rem 3rem 1.5rem 4.5rem;
+  border-bottom: 1px solid #97a0ac;
+}
+
+.modal__header h2 {
+  color: var(--black);
+  font-size: 1.5rem;
+  line-height: 1.5;
+  font-weight: bold;
+}
+
+.modal__close {
+  appearance: none;
+  border: none;
+  background-color: var(--black);
+
+  display: grid;
+  place-items: center;
+
+  width: 3rem;
+  height: 3rem;
+
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.29);
+
+  border-radius: 999px;
+
+  transition: background-color 0.3s ease-out;
+}
+
+.modal__close :deep(svg) {
+  transition: rotate 0.3s ease-out;
+}
+
+.modal__close:hover {
+  background-color: #303d63;
+  cursor: pointer;
+}
+
+.modal__close:hover :deep(svg) {
+  rotate: 90deg;
+}
+
+.modal__close :deep(svg *) {
+  color: var(---ffffff);
+}
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.modal-enter-active .modal,
+.modal-leave-active .modal {
+  transition: transform 0.3s ease-out;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal,
+.modal-leave-to .modal {
+  transform: translateY(100%);
 }
 </style>
